512f1ee3b75d824794350fe1ef9e3e9be6f4bf12
Isaac Tillema <Tillema97@gmail.com>
2021-07-06 18:31:15 -0400

    Add project files.

---
+++ Badges/BadgesProgram.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Badges
+{
+    class BadgesProgram
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Green Plan/GreenPlanProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Green_Plan
+{
+    class GreenPlanProgram
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
